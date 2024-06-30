2a71260cabe931eaa5125ea73219af80ba14dfa2
kdhol <dholariyakishan1@gmail.com>
2024-06-30 19:35:44 +0200

    06_03_b add JsonSerializables in yaml and in image_model

---
+++ chat_app/lib/models/image_model.dart
@@ -0,0 +1,31 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'image_model.g.dart';
+
+@JsonSerializable()
+class PixelformImage {
+  String id;
+  String filename;
+  String? title;
+
+  @JsonKey(name: 'url_full_size')
+  String urlFullSize;
+
+  @JsonKey(name: 'url_small_size')
+  String urlSmallSize;
+
+  PixelformImage(
+      {this.title,
+      required this.id,
+      required this.filename,
+      required this.urlFullSize,
+      required this.urlSmallSize});
+
+  /// Connect the generated [_$PersonFromJson] function to the `fromJson`
+  /// factory.
+  factory PixelformImage.fromJson(Map<String, dynamic> json) =>
+      _$PixelformImageFromJson(json);
+
+  /// Connect the generated [_$PersonToJson] function to the `toJson` method.
+  Map<String, dynamic> toJson() => _$PixelformImageToJson(this);
+}
